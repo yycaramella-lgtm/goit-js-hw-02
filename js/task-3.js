9798e017f088e32b2677511927fe14bc3e308f61
Julia K <yy.caramella@gmail.com>
2026-03-29 19:44:11 +0300

    commit hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,26 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = parseFloat(content);
-  const paddingValue = parseFloat(padding);
-  const borderValue = parseFloat(border);
+function checkForSpam(message) {
+  const normalizedMessage = message.toLowerCase();
 
-  return contentValue + paddingValue * 2 + borderValue * 2;
+  return normalizedMessage.includes("spam") || normalizedMessage.includes("sale");
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); 
+// false
+
+console.log(checkForSpam("JavaScript weekly newsletter")); 
+// false
+
+console.log(checkForSpam("Get best sale offers now!")); 
+// true
+
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+// true
+
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+// true
+
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+// true
+
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
+// true
